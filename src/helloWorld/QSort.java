121155ed4ff7054031635d56fe56d159a6101904
sunny <sunny5962@gmail.com>
2015-05-09 18:04:48 -0700

    randomQsort

--- src/helloWorld/QSort.java
+++ src/helloWorld/QSort.java
@@ -7,30 +7,48 @@ package helloWorld;
 
 public class QSort{
 	
-	private static int size=1000;
+	private static int size=1000000;
 	private static int arr[]= new int[size];
 	
 	public static void main(String[] args)
 	{
-		Double mul=new Double(10000);
-		
+		Double mul=new Double(10*size);
+		int newarr[]=new int[size];
 		for(int i=0 ; i<size; i++)
 		{
 			arr[i]= (int)(Math.random()*mul);	
+      //arr[i]= i;
+      newarr[i] = arr[i];
 		}
-		
+	
+    long t1= System.currentTimeMillis();
 		qSort();
-	
+	  long t2= System.currentTimeMillis();
 		System.out.println("***********");
-	
 		for(int i :arr)
-                System.out.println(i);
+      System.out.println(i);
+
+    long t3= System.currentTimeMillis();
+    randomQSort(newarr);
+    long t4= System.currentTimeMillis();
+    System.out.println("******Random Q Sort *****");
+    for(int i :newarr)
+      System.out.println(i);
+
+    System.out.println("Time taken by normal qsort= "+(t2-t1)+"\n"+"Time taken by randomized qsort= "+(t4-t3));
 	}
 
 	private static void qSort()
 	{
 		qSortRec(0, size-1);
 	}
+
+/**
+ * randomQSort  using   pseudo-Random generator
+ */
+  private static void randomQSort(int[] arr){
+    randomQSortRec(arr, 0, arr.length-1);
+  }
 	
 	private static void qSortRec(int start, int end)
 	{
@@ -75,6 +93,14 @@ public class QSort{
 		arr[ptr1]= arr[ptr2];
 		arr[ptr2]= temp;
 	}
+
+  private static void swap(int[] arr, int ptr1, int ptr2){
+  
+    int temp=arr[ptr1];
+    arr[ptr1]= arr[ptr2];
+    arr[ptr2]= temp;
+
+  }
 
 	private static int partition(int start, int end, int pivot)
 	{
@@ -122,4 +148,50 @@ public class QSort{
 		}
 		
 	}
+
+  /**
+   *  Random quick sort using pseudo-random no. generator
+   */
+  private static void randomQSortRec(int[] arr, int start, int end)
+  {
+    size=end-start+1;
+    if(size<=1){
+      return;
+    }
+    int pivot= findPivotRandom(arr, start, end, size);
+    int mid= partition(arr, start, end, pivot); // new index of pivot
+    randomQSortRec(arr, start, mid);
+    randomQSortRec(arr, mid+1, end);
+    return;
+
+  }
+
+  private static int findPivotRandom(int[] arr, int start, int end, int size){
+    
+    int pos= start+(int)(Math.random()*size); 
+    swap(arr, pos, end);
+    return arr[end];
+  }
+
+  private static int partition(int[] arr, int start, int end, int pivot)
+  {
+     int sptr= start-1;
+     int eptr= end;
+       
+     while(true){
+       while(arr[++sptr]<pivot);
+             
+       while(--eptr>0 && arr[eptr]>pivot);
+                  
+       if(sptr<eptr){
+                     
+         swap(arr, sptr, eptr);
+       }
+       else{
+         break;
+       }
+     }
+     swap(arr, sptr, end);
+     return sptr;
+  }
 }
