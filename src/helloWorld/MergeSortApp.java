a752c8893d41fa3416bf9979d95466b444052514
sunny <sunny5962@gmail.com>
2015-08-18 01:19:53 -0700

    added linklist merge

--- src/helloWorld/MergeSortApp.java
+++ src/helloWorld/MergeSortApp.java
@@ -4,15 +4,12 @@ public class MergeSortApp {
 
 	private static int[] a=new int[10];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
 		for(int i=0;i<10;i++)
 		{
 		a[i]=(new Double(Math.random()*100)).intValue();	
 		}//for
 		
-				
 		int temp[]=new int[10];
 		
 		recMergeSort(temp,0,a.length-1);
@@ -65,4 +62,66 @@ public class MergeSortApp {
 	}//for
 		
 	}
+
+  //linlist mergesort
+  public static Link mergeSort(Link first){
+             
+    if(first==null){
+      return null;
+    }
+    if(first.next==null){
+      return first;
+    }
+    Link mid = findMid(first);
+                                        
+    Link l1 = mergeSort(first);
+    Link l2= mergeSort(mid);
+    return merge(l1, l2);
+  }
+       
+  private static Link findMid(Link first){
+    Link current = first.next;
+    Link mid =first.next;
+    Link temp = first;
+    while(current!=null && current.next!=null){
+      current= current.next.next;
+      temp =mid;
+      mid = mid.next;
+    }         
+    temp.next =null;
+    return mid;
+  }
+            
+  private static Link merge(Link l1, Link l2){
+    if(l1==null && l2==null){
+      return null;
+    } 
+    if(l1==null){
+      return l2; 
+    }
+    if(l2==null){
+      return l1;
+    }
+    Link firstS = new Link();
+    Link current =firstS; 
+    while(l1!=null && l2!=null){
+      if(l1.num<l2.num){
+        current.next=l1;
+        l1 =l1.next;
+      } else{
+                                                                                                                                                  current.next =l2;
+                                                                                                                                                  l2= l2.next;
+                                                                                                                                                  }
+      current= current.next;
+    }
+    if(l1!=null){
+      current.next=l1;
+    } else if(l2!=null){
+      current.next =l2;
+    }
+    current = firstS.next;
+    firstS.next= null;
+    return current;
+    }
+
 }//class
