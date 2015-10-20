16a7b59fd458d8d3ddfacf0d730ca76a2f964f21
sunny <sunny5962@gmail.com>
2015-10-20 02:53:15 -0700

    LRU cache with test

--- src/helloWorld/LRUTest.java
+++ src/helloWorld/LRUTest.java
@@ -6,22 +6,45 @@ public class LRUTest{
 
   public static void main(String[] args){
       LRU<Integer, String> lru= new LRU<>(4);
-      int i=0;
-      while(i++<5){
-        lru.put(i, "Sunny"+i);
-      }
-      System.out.println(lru);
-      i=0;
-      
-      while(i++<4){
-        lru.get(i);
-      }
-      System.out.println(lru);
-      lru.put(10, "Sunny10");
-      System.out.println(lru);
+      testLRU(lru);
+      LRUCache<Integer, String> lruCache= new LRUCache<>(4);
+      testLRUCache(lruCache);
   }
 
-  
+  private static void testLRU(LRU<Integer, String> lru){
+
+    System.out.println("** using composition***");  
+    int i=0;
+    while(i++<5){
+      lru.put(i, "Sunny"+i);
+    }
+    System.out.println("size= "+lru.size()+"\n"+lru);
+    i=0;
+      
+    while(i++<4){
+      lru.get(i);
+    }
+    System.out.println(lru);
+    lru.put(10, "Sunny10");
+    System.out.println(lru);
+  }
+
+  private static void testLRUCache(LRUCache<Integer, String> lru){
+    System.out.println("** using inheritence***");
+    int i=0;
+    while(i++<5){
+      lru.put(i, "Sunny"+i);
+    }
+    System.out.println("size= "+lru.size()+"\n"+lru);
+    i=0;
+
+    while(i++<4){
+      lru.get(i);
+    }
+    System.out.println(lru);
+    lru.put(10, "Sunny10");
+    System.out.println(lru);
+  }
 }
 
 class LRU<K, V>{
@@ -53,5 +76,24 @@ class LRU<K, V>{
 
   public String toString(){
     return map.toString();
+  }
+
+  public int size(){
+    return map.size();
+  }
+}
+
+class LRUCache<K, V> extends LinkedHashMap<K, V>{
+
+  private final int size;
+
+  public LRUCache(int size){
+    super(size, 0.75f, true);
+    this.size= size;
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<K, V> eldest){
+    return size()> size;
   }
 }
