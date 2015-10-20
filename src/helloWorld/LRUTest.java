e1336293d6b4c4b750f317856a560d5a90f38004
sunny <sunny5962@gmail.com>
2015-10-20 02:32:16 -0700

    LRU cache

---
+++ src/helloWorld/LRUTest.java
@@ -0,0 +1,57 @@
+package helloWorld;
+
+import java.util.*;
+
+public class LRUTest{
+
+  public static void main(String[] args){
+      LRU<Integer, String> lru= new LRU<>(4);
+      int i=0;
+      while(i++<5){
+        lru.put(i, "Sunny"+i);
+      }
+      System.out.println(lru);
+      i=0;
+      
+      while(i++<4){
+        lru.get(i);
+      }
+      System.out.println(lru);
+      lru.put(10, "Sunny10");
+      System.out.println(lru);
+  }
+
+  
+}
+
+class LRU<K, V>{
+  
+  private final Map<K, V> map;
+  private final float loadFactor= 0.75f;
+  private final int size;
+
+  public LRU(int size){
+    this.size= size;
+    map= new LinkedHashMap<K, V>(size, loadFactor, true);
+  }
+  public void put(K key, V value){
+    if(map.size() >= size){
+      Iterator<Map.Entry<K, V>> itr= map.entrySet().iterator();
+      itr.next();
+      itr.remove();
+    }
+     map.put(key, value);
+  }
+
+  public V get(K key){
+    return map.get(key);
+  }
+
+  public V remove(K key){
+    return map.remove(key);
+  }
+
+  public String toString(){
+    return map.toString();
+  }
+}
